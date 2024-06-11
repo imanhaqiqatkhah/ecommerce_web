95e964e30816c4f24903bcce41aee760f46ff4a9
iman Haqiqat Khah <imanhaqiqatkhah@gmail.com>
2024-06-11 17:47:18 +0330

    next level for design products

--- my-project/src/components/Hero/Hero.jsx
+++ my-project/src/components/Hero/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import App from "../../App"
 import Image1 from "../../assets/hero/flyingo.png"
 import Image2 from "../../assets/hero/aircraft.png"
 import Image3 from "../../assets/hero/travel.png"
+import Slider from "react-slick"
 
 const ImageList = [
   {
@@ -28,42 +30,54 @@ const ImageList = [
 ]
 
 const Hero = () => {
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  }
   return (
-    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
+    <div className="relative overflow-hidden min-h-[425px] sm:min-h-[500px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
       {/* background pattern */}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
       {/* hero section */}
-      <div className="container pb-8 sm:pb-0">
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2">
-            {/* text content section */}
-            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-              <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold ">
-                Fly (Order Flyingo)
-              </h1>
-              <p className="text-sm">
-                any of a large number of insects characterized by the use of
-                only one pair of wings for flight and the reduction of the
-                second pair of wings to knobs used for balance.
-              </p>
-              <div>
-                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
-                  Order Now
-                </button>
+      <div className="container pb-2 sm:pb-0">
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div key={data.id}>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-8 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                  <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold ">
+                    {data.title}
+                  </h1>
+                  <p className="text-sm">{data.description}</p>
+                  <div>
+                    <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                      Order Now
+                    </button>
+                  </div>
+                </div>
+                {/* Image section */}
+                <div className="order-1 sm:order-2">
+                  <div className="relative z-10">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] lg:scale-120 sm:h-[400px] sm:w-[400px] sm:scale-110 object-contain mx-auto"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            {/* Image section */}
-            <div className="order-1 sm:order-2">
-              <div className="relative z-10">
-                <img
-                  src={Image1}
-                  alt=""
-                  className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+          ))}
+        </Slider>
       </div>
     </div>
   )
---
+++ my-project/src/components/Products/Products.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Products = () => {
+  return (
+    <div>
+      <div>{/* Header Section */}</div>
+      <div className="">
+        <p>Top Selling Products for you</p>
+        <h1>Products</h1>
+        <p>
+          Airplane wings are shaped to make air move faster over the top of the
+          wing. When air moves faster, the pressure of the air decreases
+        </p>
+      </div>
+      <div>{/* Body Section */}</div>
+    </div>
+  )
+}
+
+export default Products

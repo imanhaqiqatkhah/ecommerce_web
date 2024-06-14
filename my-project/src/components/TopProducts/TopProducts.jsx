ba4f5e71956d2484e97bc245ef3745c533e32804
iman Haqiqat Khah <imanhaqiqatkhah@gmail.com>
2024-06-14 23:25:45 +0330

    "Updated button class to change hover text color from primary to secondary"

--- my-project/src/components/TopProducts/TopProducts.jsx
+++ my-project/src/components/TopProducts/TopProducts.jsx
@@ -83,7 +83,7 @@ const TopProducts = () => {
                   {data.description}
                 </p>
                 <button
-                  className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
+                  className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-secondary"
                   // onClick={handleOrderPopup}
                 >
                   سفارش آنلاین

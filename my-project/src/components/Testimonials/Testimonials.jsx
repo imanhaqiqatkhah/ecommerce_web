388cffe3925c81939d7366395440d64275cf09a6
iman Haqiqat Khah <imanhaqiqatkhah@gmail.com>
2024-06-15 13:32:24 +0330

    "Updated Testimonials component: changed testimonial data, added AOS animation, and modified slider settings and card layout."

--- my-project/src/components/Testimonials/Testimonials.jsx
+++ my-project/src/components/Testimonials/Testimonials.jsx
@@ -4,26 +4,26 @@ import Slider from "react-slick"
 const TestimonialData = [
   {
     id: 1,
-    name: "Victor",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quaerat exercitationem. Iure iusto odit, provident amet nobis, optio dolore accusantium et reprehenderit quod eos exercitationem eaque excepturi mollitia quaerat ex?",
+    name: "ایمان",
+    text: "عالی، محصولاتتون عالین، من که خیلی خیلی راضیم",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 2,
-    name: "Satya Nadella",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quaerat exercitationem. Iure iusto odit, provident amet nobis, optio dolore accusantium et reprehenderit quod eos exercitationem eaque excepturi mollitia quaerat ex?",
+    name: "اکرم",
+    text: "عالی، عجب سایت خفن و خوشگلی، به همه آرزوهات میرسی، مطمئنم",
     img: "https://picsum.photos/102/102",
   },
   {
     id: 3,
-    name: "Virat Kohli",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quaerat exercitationem. Iure iusto odit, provident amet nobis, optio dolore accusantium et reprehenderit quod eos exercitationem eaque excepturi mollitia quaerat ex?",
+    name: "اقدس",
+    text: "من میخوام سرمایه گذاری کنم روی کسب و کارتون، به واتساپ پیام دادم، مرسی",
     img: "https://picsum.photos/103/103",
   },
   {
     id: 4,
-    name: "Sachin Tendulkar",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quaerat exercitationem. Iure iusto odit, provident amet nobis, optio dolore accusantium et reprehenderit quod eos exercitationem eaque excepturi mollitia quaerat ex?",
+    name: "امین",
+    text: "داداش گلم، همیشه پاینده باشی",
     img: "https://picsum.photos/104/104",
   },
 ]
@@ -43,7 +43,7 @@ const Testimonials = () => {
     pauseOnFocus: true,
     responsive: [
       {
-        breakpoint: 1000,
+        breakpoint: 10000,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -89,19 +89,29 @@ const Testimonials = () => {
           </p>
         </div>
         {/* Testimonial Cards */}
-        <div>
+        <div data-aos="zoom-in">
           <Slider {...settings}>
             {TestimonialData.map((data) => (
-              <div
-                key={data.id}
-                className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl"
-              >
-                <div>
-                  <img
-                    src={data.img}
-                    alt=""
-                    className="rounded-full w-20 h-20"
-                  />
+              <div key={data.id} className="my-6">
+                <div className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative">
+                  <div className="mb-4">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="rounded-full w-20 h-20"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-4" dir="rtl">
+                    <div className="space-y-3">
+                      <p className="text-xs text-gray-500">{data.text}</p>
+                      <h1 className="text-xl font-bold text-black/80 dark:text-light font-custom1">
+                        {data.name}
+                      </h1>
+                    </div>
+                  </div>
+                  <p className="text-black/20 text-9xl font-custom1 absolute top-0 right-0">
+                    ,,
+                  </p>
                 </div>
               </div>
             ))}
